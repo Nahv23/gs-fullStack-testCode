6c271d36cfc3ab54318f080be94a1915a284e98a
Nerea <nereaparicio@gmail.com>
2020-08-29 12:32:13 +0200

    Added Back history button and GS font (Scout)

--- gs-front/src/Components/PhoneScreen.jsx
+++ gs-front/src/Components/PhoneScreen.jsx
@@ -4,8 +4,15 @@ import history from '../history';
 export const PhoneScreen = () => {
 
 
-  return(
-    <h4> Phone Screen </h4>
+  return (
+    <>
+      <h4> Phone Screen </h4>
+      <div className="col-1 back-button">
+        <button type="button" cy-test={'back-button'} onClick={() => history.goBack()}><i
+          id={'back-button'} className="fas fa-arrow-left mr-1" />
+        </button>
+      </div>
+    </>
   )
 }
 
